e1925ae93e693f2e78c5c69290a989bd1933f889
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-12-04 11:35:29 +0530

    get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,4 +26,17 @@ exports.getUser = async (req, res) => {
     } catch (error) {
             res.status(400).json({ error: error.message});
         }
+};
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+
+        if (!users) {
+            return res.status(404).json({error: "No users found"});
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message});
+    }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/users", userController.createUser); // http://localhost:3000/api/users
-router.get("/users/:id",userController.getUser);
+router.get("/users/:id",userController.getUser); // http://localhost:3000/api/users/id
+router.get("/users", userController.getAllUsers);
 
 module.exports = router;
