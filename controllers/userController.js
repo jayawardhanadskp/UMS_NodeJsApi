114769d96d5a02388f0431b22915bdd301d4709c
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-12-04 12:04:25 +0530

    Added updateUser function to userController and corresponding route to userRoutes

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,4 +39,22 @@ exports.getAllUsers = async (req, res) => {
     } catch (error) {
         res.status(400).json({ error: error.message});
     }
+};
+
+exports.updateUser = async (req, res) => {
+    try {
+        
+        const user = await user.findOneAndUpdate(userId, req.body, {
+            new: true,
+            runValidators: true,
+        });
+
+        if (!user) {
+            return res.status(404).json({error: "User Not Found"});
+        }
+        res.status(200).json(user);
+        
+    } catch (error) {
+        res.status(400).json({ error: error.message});
+    }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/userController");
 router.post("/users", userController.createUser); // http://localhost:3000/api/users
 router.get("/users/:id",userController.getUser); // http://localhost:3000/api/users/id
 router.get("/users", userController.getAllUsers);
+router.post("/users/update:id", userController.updateUser);
 
 module.exports = router;
